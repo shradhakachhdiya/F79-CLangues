78adbbe10ce7ab11eda197ff1f6d1c7ebae3cb55
shradhakachhdiya <shardhdhakachhdiya@gmail.com>
2022-09-14 13:03:09 +0530

    clangue

---
+++ ASI1.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a=18,b=12;
+clrscr();
+       /*sum
+       a+=5;
+       printf("a=%d\n",a);
+
+       b-=2;
+       printf(" b=%d\n",b);
+
+       a*=3;
+       printf("a=%d\n",a);
+
+       b/=2;
+       printf("b=%d\n",b);
+
+       a%=2;
+       printf("a=%d",a);
+
+
+       b&=a;
+       printf("a=%d",a);
+
+      a^=5;
+      printf("%d",a);
+	    */
+      a|=4;
+      printf("%d",a);
+getch();
+}
